bdbcb8bd32c48c63668e28fd6f16ca258de779e2
Lewis <lewisburns630@googlemail.com>
2017-05-16 20:09:37 +0100

    Add Analogue camera to test file

--- PhotographerTest.java
+++ PhotographerTest.java
@@ -5,11 +5,13 @@ import org.junit.*;
 public class PhotographerTest{
   Photographer photographer;
   DigitalCamera digitalCamera;
+  AnalogueCamera analogueCamera;
 
   @Before 
   public void before() {
     photographer = new Photographer();
     digitalCamera = new DigitalCamera(); 
+    analogueCamera = new AnalogueCamera();
   }
 
 
